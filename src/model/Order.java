b05334322718340fc5d1a7b4d5294cf017b1f6f6
Patrick Boucicault <patrickjoshuab@gmail.com>
2017-11-22 23:54:01 -0500

    Did order class. Created class for payment and review

---
+++ src/model/Order.java
@@ -0,0 +1,65 @@
+package model;
+
+public class Order {
+
+	private int order_ID;
+	private int customer_ID;
+	private String order_status;
+	private int total;
+	private String order_date;
+	
+	public int getOrder_ID() {
+		return order_ID;
+	}
+
+	public void setOrder_ID(int order_ID) {
+		this.order_ID = order_ID;
+	}
+
+	public int getCustomer_ID() {
+		return customer_ID;
+	}
+
+	public void setCustomer_ID(int customer_ID) {
+		this.customer_ID = customer_ID;
+	}
+
+	public String getOrder_status() {
+		return order_status;
+	}
+
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public String getOrder_date() {
+		return order_date;
+	}
+
+	public void setOrder_date(String order_date) {
+		this.order_date = order_date;
+	}
+
+	public Order(int order_ID) {
+		this.order_ID = order_ID;	
+	}
+	
+	public Order(int order_ID, int customer_ID, String order_status,
+			int total, String order_date) {
+		
+		this.order_ID = order_ID;
+		this.customer_ID = customer_ID;
+		this.order_date = order_date;
+		this.order_status = order_status;
+		this.total = total;
+	}
+	
+}
